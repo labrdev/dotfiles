f774bcef64665e677521f5975a93571af72423a2
Luis Aaron Blas <92341949+blasluis@users.noreply.github.com>
2022-03-09 17:17:38 -0500

    Mis dotfiles

---
+++ shell/pw_profile.ps1
@@ -0,0 +1,14 @@
+# Prompt Starship
+$ENV:STARSHIP_CONFIG = "$HOME\.config\dotfiles\shell\prompt\starship.toml"
+$ENV:STARSHIP_DISTRO = "者 "
+Invoke-Expression (&starship init powershell)
+
+# Prompt OMP
+# Import-Module oh-my-posh
+
+#Load OMP
+# $omp_config = Join-Path $PSScriptRoot ".\labr.omp.json"
+# oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+# Set-PoshPrompt blue-owl
+
+# Import-Module -Name Terminal-Icons
